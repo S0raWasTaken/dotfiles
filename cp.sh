a15335012ae6d3a3085cb58ce46fc14122e49899
S0raWasTaken <s0ra@tuta.io>
2022-01-13 17:03:32 -0500

    Add my zsh plugins and some /etc config files

--- cp.sh
+++ cp.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 CONFIG="$HOME/.config"
+PLUGINS="$HOME/zsh-plugins"
+
+# .config dotfiles
 cp -r $CONFIG/alacritty/* config/alacritty
 cp -r $CONFIG/bspwm/* config/bspwm
 cp -r $CONFIG/flashfocus/* config/flashfocus
@@ -13,8 +16,18 @@ cp -r $CONFIG/polybar/* config/polybar
 cp -r $CONFIG/sxhkd/* config/sxhkd
 cp -r $CONFIG/PCSX2/bios/* config/PCSX2/bios
 
-#Home dotfiles
+# Home dotfiles
 cp -r $HOME/.zshrc .
 cp -r $HOME/.aliasrc .
 cp -r $HOME/.spacemacs .
 cp -r $HOME/.themerc .
+
+# zsh-plugins
+cp $PLUGINS/* zsh-plugins
+
+# /etc conf files
+cp /etc/paru.conf etc/
+cp /etc/pacman.conf etc/
+cp /etc/rc.local etc/
+cp /etc/makepkg.conf etc/
+cp /etc/proxychains.conf etc/
